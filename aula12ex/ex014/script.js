3f31b0f28772bbb3abdda140165feadb5791ac95
Ivyson Ribeiro <ribeiroivyson@yahoo.com>
2023-01-08 03:41:58 -0300

    exer014 finalizado.

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,24 @@
-alert('Olá!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >=0 && hora < 12) {
+        //Bom Dia!!
+        img.src = 'imagens/fotomanha.jpg'
+        document.body.style.background = '#89CBFB'
+    }else if(hora >= 12 && hora < 18 ) {
+        //Boa Tarde!
+        img.src = 'imagens/fototarde.jpg'
+        document.body.style.background = '#EFA866'
+    }else{
+        //Boa Noite!
+        img.src = 'imagens/fotonoite.jpg'
+        document.body.style.background = '#2E1C46'
+    }
+
+
+
+}
