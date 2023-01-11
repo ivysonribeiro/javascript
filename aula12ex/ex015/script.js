3180337497a2376d22cb860b921fa4e638f8bdfb
Ivyson Ribeiro <ribeiroivyson@yahoo.com>
2023-01-10 22:23:11 -0300

    aula12 ex015 finalizado verificador de idade

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,29 +9,37 @@ function verificar() {
     var fsex = document.getElementsByName('radsex')
     var idade = ano - Number(fano.value)
     var gênero = ''
-    var img = document.getElementById('imagem')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     if(fsex[0].checked) {
         gênero = 'Homem'
         if(idade >= 0 && idade <10){
             //
-            img.src = 'imagens/bebem.jpg'
+            img.src = 'imagens/foto-bebem.jpg'
         }else if(idade < 21){
             //jovem
+            img.src = 'imagens/foto-jovemma.jpg'
         }else if(idade < 50){
             //adulto
+            img.src = 'imagens/foto-adulto.jpg'
         }else{
             //idoso
+            img.src = 'imagens/foto-idoso.jpg'
         }
     }else if(fsex[1].checked) {
         gênero = 'Mulher'
         if(idade >= 0 && idade <10){
             //criança
+            img.src = 'imagens/foto-bebef.jpg'
         }else if(idade < 21){
             //jovem
+            img.src = 'imagens/foto-jovemf.jpg'
         }else if(idade < 50){
             //adulto
+            img.src = 'imagens/foto-adulta.jpg'
         }else{
             //idoso
+            img.src = 'imagens/foto-idosa.jpg'
         }
     }
     res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
